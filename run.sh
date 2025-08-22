dffcaff0bf9afafbf5abb733f6654cf4966d24be
Mostafa Touny <mostafatouny@protonmail.com>
2025-08-22 23:12:34 +0300

    first commit

---
+++ run.sh
@@ -0,0 +1,10 @@
+pdflatex main.tex
+bibtex main
+pdflatex main.tex
+pdflatex main.tex
+
+rm -f main.{aux,log,out,toc,fdb_latexmk,fls,bbl,blg,run.xml,bcf,blx.bib}
+rm -f main-blx.bib
+rm -fr *region*.*
+
+mv main.pdf out.pdf
