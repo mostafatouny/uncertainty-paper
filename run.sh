a94cb2205f26e1a7a888fc960fd3becff610ed4e
Mostafa Touny <mostafatouny@protonmail.com>
2025-09-23 20:38:23 +0300

    Tue Sep 23 08:38:23 PM EEST 2025

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 pdflatex main.tex
-bibtex main
-pdflatex main.tex
-pdflatex main.tex
+# bibtex main
+# pdflatex main.tex
+# pdflatex main.tex
 
 rm -f main.{aux,log,out,toc,fdb_latexmk,fls,bbl,blg,run.xml,bcf,blx.bib}
 rm -f main-blx.bib
